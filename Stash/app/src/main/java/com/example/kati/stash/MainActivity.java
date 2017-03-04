44bb589062e2522b9b6166b6cf3de45a5bfc7bb1
kw531 <katiwilliams531@gmail.com>
2017-03-04 13:08:52 -0600

    Added in delete function

--- Stash/app/src/main/java/com/example/kati/stash/MainActivity.java
+++ Stash/app/src/main/java/com/example/kati/stash/MainActivity.java
@@ -1,44 +1,33 @@
 package com.example.kati.stash;
 
-import android.app.ListActivity;
 import android.content.Intent;
 import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.Adapter;
 import android.widget.Toast;
+import android.app.ListActivity;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Hashtable;
 
-import static android.R.attr.value;
-import static android.R.attr.y;
-import static android.R.id.list;
-import static android.provider.Contacts.SettingsColumns.KEY;
-import static android.view.View.Y;
-import static com.example.kati.stash.R.id.listView;
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity{
 
     DBHandler myDB = new DBHandler(this);
-
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ListView listView = (ListView) findViewById(R.id.listView);
         registerForContextMenu(listView);
+        listView.setLongClickable(true);
 
         myDB.deleteAllYarn();
         myDB.addYarn(new Yarn(0, "Red Heart", "Comfy", "Red", "100% Acrylic", 5.3, 400.0));
@@ -53,6 +43,17 @@ public class MainActivity extends AppCompatActivity {
         myDB.addYarn(new Yarn(2, "Knit Picks", "Lux", "Purple", "100% Silk", 2, 50));
         refreshDB();
     }
+
+
+/*    @Override
+    public boolean onItemLongClick(AdapterView<?> l, View v,
+                                   final int position, long id) {
+
+        yarnID=position;
+        Toast.makeText(this, "long clicked pos: " + position, Toast.LENGTH_LONG).show();
+
+        return true;
+    }*/
 
     @Override
     public void onRestart() {
@@ -67,31 +68,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+    public void onCreateContextMenu (ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
         getMenuInflater().inflate(R.menu.yarn_longclick_menu, menu);
     }
 
+
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-        // Context menu button has been clicked
+        AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+
+        int yarnID=info.position;
+
         switch (item.getItemId()) {
             case R.id.lc_Edit:
                 Toast.makeText(this, "EDIT GOES HERE", Toast.LENGTH_LONG).show();
                 return true;
             case R.id.lc_Delete:
-                Toast.makeText(this, "DELETE GOES HERE", Toast.LENGTH_LONG).show();
-            return true;
+                removeYarn(yarnID);
+                refreshDB();
+                Toast.makeText(this, "Yarn Deleted", Toast.LENGTH_LONG).show();
+                return true;
             default:
                 return super.onContextItemSelected(item);
         }
     }
-
-
-
+    //Hashtable<Integer, String> yarnTable = new Hashtable<>();
     public void refreshDB() {
         ListView listView = (ListView) findViewById(R.id.listView);
         ArrayList<String> theList = new ArrayList<>();
+
         final Cursor data = myDB.getCursor();
         //Toast.makeText(this, "count:" + myDB.findLastID(), Toast.LENGTH_LONG).show();
 
@@ -99,6 +105,7 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "There are no contents in this list!", Toast.LENGTH_LONG).show();
         } else {
             while (data.moveToNext()) {
+                //yarnTable.put(data.getInt(0),data.getString(2));
                 theList.add(data.getString(1) + " - " + data.getString(2) + " - " + data.getString(3));
                 ListAdapter listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1, theList);
                 listView.setAdapter(listAdapter);
@@ -108,7 +115,6 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //parent.getItemAtPosition(position)
                 int rowID=(int)parent.getItemIdAtPosition(position);
                 Toast.makeText(getBaseContext(), "ID sent: " + rowID  + "", Toast.LENGTH_LONG).show();
 
@@ -117,8 +123,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
     }
 
 
+    public void removeYarn(int ID){
+        myDB.deleteYarn(myDB.getYarn(ID));
+    }
 }
 
